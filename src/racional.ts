7de526fc45cf4914bf922ebff5e59b4b735e42bb
netoalanis <netoalanis@erasistemas.com>
2018-02-21 18:37:47 -0800

    Operaciones Completas
    Agrege las operaciones faltantes
    resta, multiplicacion, division y reduccion

--- src/racional.ts
+++ src/racional.ts
@@ -33,6 +33,64 @@ namespace numeros {
             return rr;
         }
 
+        // Resta
+        static resta(r1:Racional,r2:Racional):Racional{
+            var rr:Racional;
+            var nr,dr:number;
+
+            nr = (r1.n * r2.d) - (r1.d * r2.n);
+            dr = (r1.d * r2.d);
+
+            rr = new Racional(nr,dr);
+
+            return rr;
+        }
+
+        // Multiplicacion
+        static multiplicacion(r1:Racional,r2:Racional):Racional{
+            var rr:Racional;
+            var nr,dr:number;
+
+            nr = (r1.n * r2.n);
+            dr = (r1.d * r2.d);
+
+            rr = new Racional(nr,dr);
+
+            return rr;
+        }
+
+        // Division
+         static division(r1:Racional,r2:Racional):Racional{
+            var rr:Racional;
+            var nr,dr:number;
+
+            nr = (r1.n * r2.d);
+            dr = (r1.d * r2.n);
+
+            rr = new Racional(nr,dr);
+
+            return rr;
+        }
+
+        // Reduccion a la minima expresion con el algoritmo de euclides
+        reducir():void{
+            var r0,r1,r2:number;
+
+            r0 = this.n;
+            r1 = this.d;
+
+            do {
+                r2 = r0%r1
+                r0 = r1;
+                r1 = r2;
+            } while (r2!=0);
+
+            this.n = this.n/r0;
+            this.d = this.d/r0;
+
+            return;
+        }
+
         // hacer las funciones de resta, mult, div
         // hacer la funcion reducir
 
